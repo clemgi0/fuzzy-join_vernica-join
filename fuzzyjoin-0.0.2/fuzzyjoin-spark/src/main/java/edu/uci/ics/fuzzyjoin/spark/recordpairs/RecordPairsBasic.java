bf0a9e492601edbd39508b411bde83e33097c25f
clemgi0 <clement.giraudon2@gmail.com>
2024-08-29 08:34:12 +0200

    add phase 2 reduce of stage 3

--- fuzzyjoin-0.0.2/fuzzyjoin-spark/src/main/java/edu/uci/ics/fuzzyjoin/spark/recordpairs/RecordPairsBasic.java
+++ fuzzyjoin-0.0.2/fuzzyjoin-spark/src/main/java/edu/uci/ics/fuzzyjoin/spark/recordpairs/RecordPairsBasic.java
@@ -16,7 +16,8 @@ import edu.uci.ics.fuzzyjoin.spark.recordpairs.selfjoin.SelfJoinPhase1Reduce;
 import scala.Tuple2;
 
 public class RecordPairsBasic {
-    public static void main(JavaSparkContext sc, JavaRDD<String> records, JavaRDD<String> ridPairs) throws IOException {
+    public static JavaRDD<String> main(JavaSparkContext sc, JavaRDD<String> records, JavaRDD<String> ridPairs)
+            throws IOException {
         //
         // -------------------- PHASE 1 --------------------
         //
@@ -30,25 +31,30 @@ public class RecordPairsBasic {
 
             JavaRDD<String> union = records.union(ridPairs);
 
-            // showRDD1(union);
+            // showRDD1(ridPairs);
 
-            LogUtil.logStage("Self-join : Map");
+            LogUtil.logStage("Phase 1 : Self-join : Map");
 
             JavaPairRDD<String, Integer> mappedData = union.flatMapToPair(new SelfJoinPhase1Map());
             JavaPairRDD<Integer, String> mappedDataInverted = mappedData.mapToPair(Tuple2::swap);
 
-            showPairRDD1(mappedDataInverted);
+            // showPairRDD1(mappedDataInverted);
 
-            LogUtil.logStage("Self-join : Reduce");
+            LogUtil.logStage("Phase 1 : Self-join : Reduce");
 
             JavaPairRDD<Integer, Iterable<String>> mappedDataInvertedGrouped = mappedDataInverted.groupByKey();
             List<Tuple2<Integer, Iterable<String>>> mappedDataIterable = mappedDataInvertedGrouped.collect();
 
             ArrayList<Tuple2<IntPair, String>> halfRecordsList = new ArrayList<>();
             for (Tuple2<Integer, Iterable<String>> tuple : mappedDataIterable) {
-                System.out.println(tuple._1() + " " + tuple._2());
+                // LogUtil.logStage("");
+                // System.out.println(tuple._1() + " " + tuple._2());
                 halfRecordsList.addAll(new SelfJoinPhase1Reduce().call(tuple));
             }
+
+            // System.out.println("\nHalf records list:");
+            // halfRecordsList.forEach(r -> System.out
+            // .println(r._1().getFirst() + " " + r._1().getSecond() + " " + r._2()));
 
             halfRecords = sc.parallelizePairs(halfRecordsList);
         } else {
@@ -59,23 +65,29 @@ public class RecordPairsBasic {
             halfRecords = new JavaPairRDD<>(null, null, null);
         }
 
-        showPairRDD2(halfRecords);
+        // showPairRDD2(halfRecords);
 
         //
         // -------------------- PHASE 2 --------------------
         //
-
         if (sc.getConf().get(Main.DATA_SUFFIX_INPUT_PROPERTY).isEmpty()) {
-            //
-            // self-join
-            //
-
+            LogUtil.logStage("Phase 2 : Self-join : Reduce");
         } else {
-            //
-            // R-S join
-            //
-
+            LogUtil.logStage("Phase 2 : R-S join : Reduce");
         }
+
+        // Reduce, no map needed
+        JavaPairRDD<IntPair, String> mapPairedRecordsRDD = halfRecords.reduceByKey(new ReducePhase2(sc));
+        JavaRDD<String> pairedRecordsRDD = mapPairedRecordsRDD.values();
+        JavaRDD<String> pairedRecordsSortedRDD = pairedRecordsRDD.sortBy((String s) -> s, true, 1);
+
+        // Show final results
+        String[] pairedRecords = pairedRecordsSortedRDD.collect().toArray(new String[0]);
+        for (String pairedRecord : pairedRecords) {
+            System.out.println(pairedRecord);
+        }
+
+        return pairedRecordsSortedRDD;
     }
 
     public static void showRDD1(JavaRDD<String> rdd) {
--- fuzzyjoin-0.0.2/fuzzyjoin-spark/src/main/java/edu/uci/ics/fuzzyjoin/spark/recordpairs/ReducePhase2.java
+++ fuzzyjoin-0.0.2/fuzzyjoin-spark/src/main/java/edu/uci/ics/fuzzyjoin/spark/recordpairs/ReducePhase2.java
@@ -1,5 +1,53 @@
 package edu.uci.ics.fuzzyjoin.spark.recordpairs;
 
-public class ReducePhase2 {
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
 
+import edu.uci.ics.fuzzyjoin.FuzzyJoinConfig;
+import edu.uci.ics.fuzzyjoin.spark.Main;
+
+public class ReducePhase2 implements Function2<String, String, String> {
+    private final boolean isSelfJoin;
+
+    public ReducePhase2(JavaSparkContext sc) {
+        isSelfJoin = "".equals(sc.getConf().get(Main.DATA_SUFFIX_INPUT_PROPERTY, ""));
+    }
+
+    @Override
+    public String call(String record1, String record2) throws Exception {
+        String records[] = new String[2];
+        String splits[] = record1.split(FuzzyJoinConfig.RECORD_EXTRA_SEPARATOR_REGEX);
+        String sim = splits[0];
+        records[0] = splits[1];
+
+        int rid1, rid2;
+        rid1 = Integer.valueOf(records[0].split(FuzzyJoinConfig.RECORD_SEPARATOR_REGEX)[FuzzyJoinConfig.RECORD_KEY]);
+
+        records[1] = record2.split(FuzzyJoinConfig.RECORD_EXTRA_SEPARATOR_REGEX)[1];
+        rid2 = Integer
+                .valueOf(records[1]
+                        .split(FuzzyJoinConfig.RECORD_SEPARATOR_REGEX)[FuzzyJoinConfig.RECORD_KEY]);
+
+        // if ((isSelfJoin && rid1 != rid2)
+        // || (!isSelfJoin && !records[0].equals(records[1]))) {
+        // /*
+        // * if you want to have correct job counters (i.e. map output
+        // * records = reduce input records)
+        // */
+        // // while (values.hasNext()) {
+        // // values.next();
+        // // }
+        // break;
+        // }
+
+        int i0 = 0, i1 = 1;
+        if (isSelfJoin && rid1 > rid2) {
+            i0 = 1;
+            i1 = 0;
+        }
+
+        return records[i0] + FuzzyJoinConfig.RECORD_EXTRA_SEPARATOR + sim
+                + FuzzyJoinConfig.RECORD_EXTRA_SEPARATOR
+                + records[i1];
+    }
 }
